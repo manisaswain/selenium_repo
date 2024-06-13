eb0e1404e93418a1c4c286832c65127e50bc6aa7
suraj <suraj@192.168.1.150>
2024-06-13 11:16:51 +0530

    demo test executed

---
+++ seleniumProject/src/test/java/sample/Demo_Test.java
@@ -0,0 +1,11 @@
+package sample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo_Test {
+@Test
+public void test() {
+	Reporter.log("demo test executed", true);
+}
+}
